e303d28b7925d7638572252cb105300c0395dce4
Justin Thomas <justin@chromatikmusic.com>
2012-01-25 10:57:51 -0800

    Load average and CPU utilization working

--- example.js
+++ example.js
@@ -3,9 +3,8 @@
 var express = require('express');
 var app = express.createServer();
 var informant = require('./');
-//var store = new informant.MemStore();
-var store = new informant.MongoStore();
-console.log(store);
+var store = new informant.MemStore();
+//var store = new informant.MongoStore();
 app.use(informant.middleware(store));
 
 app.get('/',function(req,res) {
