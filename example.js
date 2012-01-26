220834d1dd8f8403ad7f31026d277563e957e11e
Justin Thomas <justin@chromatikmusic.com>
2012-01-26 15:12:47 -0800

    Last one..

--- example.js
+++ example.js
@@ -3,8 +3,8 @@
 var express = require('express');
 var app = express.createServer();
 var informant = require('./');
-var store = new informant.MongoStore();
-//var store = new informant.MemStore();
+//var store = new informant.MongoStore();
+var store = new informant.MemStore();
 app.use(informant.middleware());
 
 app.get('/',function(req,res) {
